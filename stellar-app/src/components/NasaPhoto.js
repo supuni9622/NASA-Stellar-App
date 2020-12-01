0b358fc1348f7ae12a413e9eca5a8955e3f3f68c
Supuni22 <supunimanamperi@gmail.com>
2020-12-01 23:06:48 +0530

    Created NasaPhoto component

--- stellar-app/src/components/NasaPhoto.js
+++ stellar-app/src/components/NasaPhoto.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, {useState, useEffect, useCallback} from 'react';
+
+// Use hided api key in .env file
+const apiKey = process.env.REACT_APP_NASA_KEY;
 
 const NasaPhoto = () => {
+
+    const [photoData, setPhotoData] = useState(null);
+
+    const fetchPhoto = useCallback(async() => {
+           const res = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`);
+
+           // Convert response to json type
+           const data = await res.json();
+           setPhotoData(data);
+
+        },[setPhotoData]);
+
+    useEffect(()=> {
+        fetchPhoto()
+    },[])
+
+    // Stop creating any error 
+    if(!photoData) return <div/>;
+
+    console.debug(photoData);
+
     return (
         <div>
-            NasaPhoto
+            {photoData.media_type === 'image' ? 
+                <img src={photoData.url} alt={photoData.title}/>
+            : <iframe
+                title='space-video'
+                src={photoData.url}
+                frameBorder='0'
+                gesture='media'
+                allow='encrypted-media'
+                allowFullScreen
+                className='photo'
+                />
+            }
+            <div>
+                <h1>{photoData.title}</h1>
+                <p>{photoData.date}</p>
+                <p>{photoData.explanation}</p>
+            </div>
         </div>
     )
 }
